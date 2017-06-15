ee1b1bf82eb02145a8b1c316d7df093ce74c94c3
MullerCedric <muller.cedric.96@gmail.com>
2017-06-15 13:02:03 +0200

    Add config files

---
+++ configs/config.php
@@ -0,0 +1,3 @@
+<?php session_start();
+    define( 'DB_FILE', 'configs/DB.ini' );
+    define('HARDCODED_URL', 'http://hepl.app/php/library/');
---
+++ configs/routes.php
@@ -0,0 +1,51 @@
+<?php /* METHOD / resource (controller name) / action (method name) */
+return [
+    /* USER */
+    'signUp' => 'GET/user/signUp',
+    'signedUp' => 'POST/user/signedUp',
+    'logIn' => 'GET/user/logIn',
+    'loggedIn' => 'POST/user/loggedIn',
+    'loggedOut' => 'GET/user/loggedOut',
+    'block' => 'POST/user/block',
+    'profile' => 'GET/user/profile',
+
+    /* AUTHORS */
+    'authors' => 'GET/authors/list',
+    'author' => 'GET/authors/zoom',
+    'addAuthor' => 'GET/authors/add',
+    'addedAuthor' => 'POST/authors/added',
+    'editAuthor' => 'GET/authors/edit',
+    'editedAuthor' => 'POST/authors/edited',
+
+    /* BOOKS */
+    'books' => 'GET/books/list',
+    'book' => 'GET/books/zoom',
+    'addBook' => 'GET/books/add',
+    'addedBook' => 'POST/books/added',
+    'editBook' => 'GET/books/edit',
+    'editedBook' => 'POST/books/edited',
+
+    /* COMMENTS */
+    'addedComment' => 'POST/comments/added',
+    'editComment' => 'GET/comments/edit',
+    'editedComment' => 'POST/comments/edited',
+    'moderateComments' => 'GET/comments/moderate',
+
+    /* BORROWINGS */
+    'borrowings' => 'GET/borrowings/list',
+    'addBorrowing' => 'GET/borrowings/add',
+    'addedBorrowing' => 'GET/borrowings/added',
+
+    /* GENRES */
+    'genres' => 'GET/genres/list',
+    'addGenre' => 'GET/genres/add',
+    'addedGenre' => 'POST/genres/added',
+
+    /* SUBSCRIPTIONS */
+    'addSubscription' => 'GET/subscriptions/add',
+    'addedSubscription' => 'POST/subscriptions/added',
+
+    /* PAGES */
+    'default' => 'GET/page/index',
+    'dashboard' => 'GET/page/dashboard',
+];
